f897e359d24d622d8a2c8414d5898e272c436fd7
idonans <idonans@126.com>
2017-07-15 20:50:13 +0800

    add amap dependency

---
+++ app/src/main/java/com/ddup4/autonav/Constants.java
@@ -0,0 +1,14 @@
+package com.ddup4.autonav;
+
+/**
+ * Created by idonans on 2017/7/15.
+ */
+
+public interface Constants {
+
+    /**
+     * 高德 key
+     */
+    String AMAP_KEY = "94d389049180ee8ae3c651092933c91d";
+
+}
