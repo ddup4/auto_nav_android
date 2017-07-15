c07dc1c9670a8f956549ef004420505d446be157
idonans <idonans@126.com>
2017-07-15 15:15:44 +0800

    init

---
+++ app/src/main/java/com/ddup4/autonav/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ddup4.autonav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
