c7bc3989c4f79e0c3e329b3205e66331e2550692
idonans <idonans@126.com>
2017-07-16 11:36:20 +0800

    add auto nav

--- app/src/main/java/com/ddup4/autonav/module/main/MainFragment.java
+++ app/src/main/java/com/ddup4/autonav/module/main/MainFragment.java
@@ -6,10 +6,30 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.amap.api.navi.AMapNavi;
+import com.amap.api.navi.AMapNaviListener;
+import com.amap.api.navi.AMapNaviView;
+import com.amap.api.navi.AMapNaviViewListener;
+import com.amap.api.navi.AMapNaviViewOptions;
+import com.amap.api.navi.model.AMapLaneInfo;
+import com.amap.api.navi.model.AMapNaviCameraInfo;
+import com.amap.api.navi.model.AMapNaviCross;
+import com.amap.api.navi.model.AMapNaviInfo;
+import com.amap.api.navi.model.AMapNaviLocation;
+import com.amap.api.navi.model.AMapNaviTrafficFacilityInfo;
+import com.amap.api.navi.model.AMapServiceAreaInfo;
+import com.amap.api.navi.model.AimLessModeCongestionInfo;
+import com.amap.api.navi.model.AimLessModeStat;
+import com.amap.api.navi.model.NaviInfo;
+import com.autonavi.tbt.TrafficFacilityInfo;
 import com.ddup4.autonav.R;
 import com.ddup4.autonav.app.BaseFragment;
+import com.okandroid.boot.AppContext;
 import com.okandroid.boot.app.ext.dynamic.DynamicViewData;
 import com.okandroid.boot.util.IOUtil;
+import com.okandroid.boot.util.ViewUtil;
+
+import java.io.IOException;
 
 /**
  * Created by idonans on 2017/2/3.
@@ -38,6 +58,24 @@ public class MainFragment extends BaseFragment<MainViewProxy> implements MainVie
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mContent != null) {
+            mContent.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mContent != null) {
+            mContent.onPause();
+        }
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         IOUtil.closeQuietly(mContent);
@@ -48,13 +86,234 @@ public class MainFragment extends BaseFragment<MainViewProxy> implements MainVie
     public void onUpdateContentViewIfChanged() {
     }
 
-    private class Content extends ContentViewHelper {
+    private class Content extends ContentViewHelper implements AMapNaviListener, AMapNaviViewListener {
+
+        protected AMapNaviView mAMapNaviView;
+        protected AMapNavi mAMapNavi;
 
         private Content(@NonNull Activity activity, @NonNull LayoutInflater inflater, @NonNull ViewGroup contentView) {
             super(activity, inflater, contentView, R.layout.ddup4_autonav_module_main_view);
 
+            mAMapNavi = AMapNavi.getInstance(AppContext.getContext());
+            mAMapNavi.addAMapNaviListener(this);
+
+            mAMapNaviView = ViewUtil.findViewByID(mRootView, R.id.navi_view);
+            mAMapNaviView.onCreate(null);
+            mAMapNaviView.setAMapNaviViewListener(this);
+
+            AMapNaviViewOptions options = mAMapNaviView.getViewOptions();
+            options.setSettingMenuEnabled(true);
+            mAMapNaviView.setViewOptions(options);
         }
 
+        protected void onResume() {
+            mAMapNaviView.onResume();
+        }
+
+        protected void onPause() {
+            mAMapNaviView.onPause();
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+
+            mAMapNaviView.onDestroy();
+            mAMapNavi.stopNavi();
+            mAMapNavi.destroy();
+        }
+
+        @Override
+        public void onInitNaviFailure() {
+
+        }
+
+        @Override
+        public void onInitNaviSuccess() {
+
+        }
+
+        @Override
+        public void onStartNavi(int i) {
+
+        }
+
+        @Override
+        public void onTrafficStatusUpdate() {
+
+        }
+
+        @Override
+        public void onLocationChange(AMapNaviLocation aMapNaviLocation) {
+
+        }
+
+        @Override
+        public void onGetNavigationText(int i, String s) {
+
+        }
+
+        @Override
+        public void onEndEmulatorNavi() {
+
+        }
+
+        @Override
+        public void onArriveDestination() {
+
+        }
+
+        @Override
+        public void onCalculateRouteFailure(int i) {
+
+        }
+
+        @Override
+        public void onReCalculateRouteForYaw() {
+
+        }
+
+        @Override
+        public void onReCalculateRouteForTrafficJam() {
+
+        }
+
+        @Override
+        public void onArrivedWayPoint(int i) {
+
+        }
+
+        @Override
+        public void onGpsOpenStatus(boolean b) {
+
+        }
+
+        @Override
+        public void onNaviInfoUpdate(NaviInfo naviInfo) {
+
+        }
+
+        @Override
+        public void onNaviInfoUpdated(AMapNaviInfo aMapNaviInfo) {
+
+        }
+
+        @Override
+        public void updateCameraInfo(AMapNaviCameraInfo[] aMapNaviCameraInfos) {
+
+        }
+
+        @Override
+        public void onServiceAreaUpdate(AMapServiceAreaInfo[] aMapServiceAreaInfos) {
+
+        }
+
+        @Override
+        public void showCross(AMapNaviCross aMapNaviCross) {
+
+        }
+
+        @Override
+        public void hideCross() {
+
+        }
+
+        @Override
+        public void showLaneInfo(AMapLaneInfo[] aMapLaneInfos, byte[] bytes, byte[] bytes1) {
+
+        }
+
+        @Override
+        public void hideLaneInfo() {
+
+        }
+
+        @Override
+        public void onCalculateRouteSuccess(int[] ints) {
+
+        }
+
+        @Override
+        public void notifyParallelRoad(int i) {
+
+        }
+
+        @Override
+        public void OnUpdateTrafficFacility(AMapNaviTrafficFacilityInfo aMapNaviTrafficFacilityInfo) {
+
+        }
+
+        @Override
+        public void OnUpdateTrafficFacility(AMapNaviTrafficFacilityInfo[] aMapNaviTrafficFacilityInfos) {
+
+        }
+
+        @Override
+        public void OnUpdateTrafficFacility(TrafficFacilityInfo trafficFacilityInfo) {
+
+        }
+
+        @Override
+        public void updateAimlessModeStatistics(AimLessModeStat aimLessModeStat) {
+
+        }
+
+        @Override
+        public void updateAimlessModeCongestionInfo(AimLessModeCongestionInfo aimLessModeCongestionInfo) {
+
+        }
+
+        @Override
+        public void onPlayRing(int i) {
+
+        }
+
+        @Override
+        public void onNaviSetting() {
+        }
+
+        @Override
+        public void onNaviCancel() {
+            Activity activity = getAvailableActivity();
+            if (activity != null) {
+                activity.finish();
+            }
+        }
+
+        @Override
+        public boolean onNaviBackClick() {
+            return false;
+        }
+
+        @Override
+        public void onNaviMapMode(int i) {
+
+        }
+
+        @Override
+        public void onNaviTurnClick() {
+
+        }
+
+        @Override
+        public void onNextRoadClick() {
+
+        }
+
+        @Override
+        public void onScanViewButtonClick() {
+
+        }
+
+        @Override
+        public void onLockMap(boolean b) {
+
+        }
+
+        @Override
+        public void onNaviViewLoaded() {
+
+        }
     }
 
 }
