1c2a5368632f08f4e6a6f23c703e5a7fa8b7e6e3
idonans <idonans@126.com>
2017-07-21 00:29:12 +0800

    add multi dex

--- app/src/main/java/com/ddup4/autonav/AppApplication.java
+++ app/src/main/java/com/ddup4/autonav/AppApplication.java
@@ -1,6 +1,8 @@
 package com.ddup4.autonav;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.squareup.leakcanary.LeakCanary;
 
@@ -9,6 +11,13 @@ import com.squareup.leakcanary.LeakCanary;
  */
 
 public class AppApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        MultiDex.install(this);
+    }
 
     @Override
     public void onCreate() {
